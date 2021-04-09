e1981f379e363121952e24e4d8704345f6609a6f
Sergio Bonato <asbonato@gmail.com>
2021-04-08 21:53:55 -0300

    Apostila 12 terminada

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Cliente } from './clientes/cliente.model';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,10 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'cadcli01';
+  clientes: Cliente[] = [];
+
+  onClienteAdicionado(cliente: Cliente) {
+    this.clientes = [...this.clientes, cliente];
+    console.log(this.clientes);
+  }
 }
